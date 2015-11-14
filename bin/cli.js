b21b53f07042935f33066f3f627a9714bb2c1015
Ryan Ramage <ryan.ramage@gmail.com>
2015-11-14 15:26:16 -0600

    better error reporting

--- bin/cli.js
+++ bin/cli.js
@@ -4,6 +4,15 @@ var path = require('path')
 
 var projectDir = path.resolve(process.argv[2])
 
-check(projectDir, function (err) {
-  if (err) console.log(err)
+check(projectDir, function (errors) {
+  if (!errors || !errors.length) return;
+
+  errors.forEach(function (err) {
+    var msg = 'is missing'
+    if (err.required) {
+      msg = 'does not satisfy ' + err.required
+    }
+    console.warn('dependency', err.dependency, msg)
+  })
+  console.warn('Solution: run `npm install`')
 })
